4abd7e475d35169d83735ef3b7681654386b79a8
Tim Kilian <timkilian51@gmail.com>
2016-03-15 16:29:09 +0100

    updated main template: handle input reading

--- CppTemplates/main.cpp
+++ CppTemplates/main.cpp
@@ -7,12 +7,27 @@ using namespace std;
 #define iterate(i, n) repeat(i, 0, n)
 #define decline(i, a, b) for (int i=a-1; i>=b; i--)
 
+#define readChar(n) if (scanf("%c", &n)!=1) readError()
+#define readCharArr(arr, n) iterate(i, n) readChar(arr[i])
+#define readString(n) if (scanf("%s", &n)!=1) readError()
+#define readStringArr(arr, n) iterate(i, n) readString(arr[i])
+#define readInt(n) if (scanf("%d", &n)!=1) readError()
+#define readIntArr(arr, n) iterate(i, n) readInt(arr[i])
+#define readLong(n) if (scanf("%ld", &n)!=1) readError()
+#define readLongLong(n) if (scanf("%lld", &n)!=1) readError()
+#define readFloat(n) if (scanf("%f", &n)!=1) readError()
+#define readDouble(n) if(scanf("%lf", &n)!=1) readError()
+#define readError() printf("failed to read\n")
+
+#define length(arr) sizeof(arr)/sizeof(arr[0])
+#define printArr(arr) iterate(i, length(arr)) cout << arr[i] << " "
+
 typedef long long int ll;
 typedef unsigned long long int ull;
 typedef vector<int> vi;
 
 const int MOD = 1e9+7;
-bool period = false;
+bool period = true;
 
 void run();
 void solution() {
@@ -30,5 +45,5 @@ void run() {
     //scanf("%d", &T);
     while (T-->0)
         solution();
-    if (period) cout << double(clock()-begin_time)/CLOCKS_PER_SEC << endl;
+    if (period) printf("[%lf]", double(clock()-begin_time)/CLOCKS_PER_SEC);
 }
